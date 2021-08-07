5c52cf7f1aff25ff5a6b874ffc1ded0d9f05b8a2
Abhishek Jain <jainabhi24feb@gmail.com>
2021-08-07 21:52:20 +0530

    Added mysql db

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# order-generator-sockets
+# Realtime Transaction Generator
+
+##### Tech-stack:
+- Server: nodejs
+- Client: javascript
+- Database: MySQL
+
+##### Reason for using web-sockets:
+- Bi-directional
+- Extensible
+- multiple conncurrent connections
+
+Steps to execute:
+1) clone this repo
+2) need mysql server locally
+3) install nodejs
+4) execute `npm install`
+5) create the db `nodemysql`
+	- you can also use `http://localhost:80/createdb` by commenting the `database : 'nodemysql'` statement in connection section. Revert it after creating the db.)
+	- create the table using `http://localhost:80/createtable`
+6) run `node app.js`
+7) connect to server at `http://localhost:80/`
--- app.js
+++ app.js
@@ -2,12 +2,48 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 const faker = require('faker');
+const mysql = require('mysql');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-//var numClients = 0;
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root123',
+    database : 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+
+// Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        //console.log(result);
+        res.send('Database created...');
+    });
+});
+
+// Create table
+app.get('/createtable', (req, res) => {
+    let sql = 'CREATE TABLE orders(id INT AUTO_INCREMENT, user VARCHAR(255), merchant VARCHAR(255), created_at DATETIME, amount REAL, currency VARCHAR(20), product_id INT, quantity INT, PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        //console.log(result);
+        res.send('table created...');
+    });
+});
 
 io.on('connection', function(socket) {
 	var interval = setInterval(function(){
